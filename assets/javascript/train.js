88592514584fca1cab7250eb0435a4426f26edcc
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-06-30 18:16:27 -0400

    more

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -14,13 +14,18 @@
 
   var database = firebase.database();
 
+//   displaying current time in military
+  setInterval(function(){
+$(".current-time").html(moment().format("HH:mm:ss"))
+  }, 1000);
+
 // create a button for adding new train
 $("#add-train").on("click", function(event) {
     console.log("hey");
     event.preventDefault();
 
 
-// creating variables for grabbing the user input with 
+// creating variables for grabbing the user input using jquery
 var trainName = $("#trainName").val().trim()
 var trainDestination = $("#trainDestination").val().trim()
 var firstTrain = $("#firstTrain").val().trim()
@@ -33,7 +38,7 @@ var trainFrequency = $("#trainFrequency").val().trim()
 //     trainFrequency
     
 // };
-
+// uploads employee data to database using .psh method
 database.ref().push({
    trainName,
     trainDestination,
@@ -42,12 +47,34 @@ database.ref().push({
     
 });
 
+// consolelog users input
+console.log(trainName);
+console.log(trainDestination);
+console.log(firstTrain);
+console.log(trainFrequency);
 
+// alert users of added train
+alert("Your train is on the way");
+
+// clearing text-boxes
+$("#trainName").val("");
+$("#trainDestination").val("");
+$("#firstTrain").val("");
+$("#trainFrequency").val("");
+
+
+// 
 database.ref().on("child_added", function(childData) {
 console.log(childData.val());
 
-var trainName = childData.val().trainName;
-var trainDestination = childData.val().trainDestination;
+childData.val().trainName;
+childData.val().trainDestination;
+childData.val().firstTrain;
+childData.val().trainFrequency;
+
+
+// append new train data into table
+$("#train-table > tbody").append("<tr><td>" + trainName + "<tr><td>" + trainDestination + "<tr><td>" + firstTrain + "<tr><td>" + trainFrequency);
 
 });
 
